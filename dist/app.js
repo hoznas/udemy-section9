9992e79e27211410ff5a7ee078669dfd652ece67
hoznas <s_unkun@yahoo.co.jp>
2022-11-19 16:35:46 +0900

    lecture 126

---
+++ dist/app.js
@@ -0,0 +1,71 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function autobind(_target, _methodName, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.mandayInputElement = this.element.querySelector('#manday');
+        this.configure();
+        this.attach();
+    }
+    validation(str) {
+        return str.trim().length != 0;
+    }
+    gatherUnerInput() {
+        const title = this.titleInputElement.value;
+        const description = this.descriptionInputElement.value;
+        const manday = this.mandayInputElement.value;
+        if (this.validation(title) && this.validation(description) && this.validation(manday)) {
+            return [title, description, +manday];
+        }
+        else {
+            alert("invalid input!");
+        }
+    }
+    clearInput() {
+        this.titleInputElement.value = "";
+        this.descriptionInputElement.value = "";
+        this.mandayInputElement.value = "";
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        const userInput = this.gatherUnerInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, manday] = userInput;
+            console.log(title, desc, manday);
+            this.clearInput();
+        }
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler);
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+__decorate([
+    autobind
+], ProjectInput.prototype, "submitHandler", null);
+const projInput = new ProjectInput();
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,80 @@
-// Code goes here!
+//autobind decorator
+function autobind(_target: any, _methodName:string, descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get(){
+            const boundFn = originalMethod.bind(this)
+            return boundFn
+        }
+    }
+    return adjDescriptor
+}
+
+class ProjectInput{
+    templateElement : HTMLTemplateElement
+    hostElement : HTMLDivElement
+    element: HTMLFormElement
+    titleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
+    mandayInputElement: HTMLInputElement
+
+    constructor(){
+        this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement
+        this.hostElement = document.getElementById("app")! as HTMLDivElement
+
+        const importedNode = document.importNode(this.templateElement.content,true)
+        this.element = importedNode.firstElementChild as HTMLFormElement
+        this.element.id = 'user-input'
+
+        this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement
+        this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement
+        this.mandayInputElement = this.element.querySelector('#manday')! as HTMLInputElement
+
+        this.configure()
+        this.attach()
+    }   
+
+    private validation(str: string): boolean{
+        return str.trim().length != 0
+    }
+
+    private gatherUnerInput():[string,string,number]|void{
+        const title = this.titleInputElement.value
+        const description  = this.descriptionInputElement.value
+        const manday = this.mandayInputElement.value
+        if(this.validation(title) && this.validation(description) && this.validation(manday)){
+            return [title,description,+manday]
+        }else{
+            alert("invalid input!")
+        }
+    }
+
+    private clearInput(){
+        this.titleInputElement.value = ""
+        this.descriptionInputElement.value = ""
+        this.mandayInputElement.value = ""
+    }
+
+    @autobind
+    private submitHandler(event: Event){
+        event.preventDefault()
+        const userInput = this.gatherUnerInput()
+        if (Array.isArray(userInput)){
+            const [title,desc,manday] = userInput
+            console.log(title,desc,manday)
+            this.clearInput()
+        }
+    }
+
+    private configure(){
+        this.element.addEventListener('submit',this.submitHandler)
+    }
+
+    private attach(){
+        this.hostElement.insertAdjacentElement('afterbegin', this.element)
+    }
+}
+
+
+const projInput = new ProjectInput()
